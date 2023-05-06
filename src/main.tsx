6b4501508a3a5ca4b78a2724b0da6965e195e1bd
H <jxhunew@gmail.com>
2023-05-06 15:03:21 +0800

    init commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+// 一个 import 代表一个 HTTP 请求
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
