2db896e2adfd3d477e08a40b7112573552ca4763
H <jxhunew@gmail.com>
2023-05-08 13:17:41 +0800

    test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import "./index.scss";
 
 const h = "hhh";
+const hh = h + h + h;
 export function Header() {
-  return <p className="header">This is Header {h}</p>;
+  return <p className="header">This is Header {hh}</p>;
 }
