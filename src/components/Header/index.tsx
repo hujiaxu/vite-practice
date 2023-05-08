b719cf4d88cb87503076287b421f04aa00585926
H <jxhunew@gmail.com>
2023-05-08 16:54:29 +0800

    processing static resources

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,16 @@
 import "./index.scss";
 import { ReactComponent as ReactLogo } from "@assets/react.svg";
 import Worker from "./worker-example.js?worker";
+import SvgIcon from "../SvgIcon";
+
+// 批量引入图片
+const icons = import.meta.glob("@assets/react*.svg", { eager: true });
+// 转换为路径
+const iconUrls = Object.values(icons).map((mod) => {
+  const fileName = mod.default.split("/").pop();
+  const [svgName] = fileName.split(".");
+  return svgName;
+});
 
 const worker = new Worker();
 worker.addEventListener("message", (e) => {
@@ -11,7 +21,10 @@ const hh = h + h + h;
 export function Header() {
   return (
     <p className="header">
-      <ReactLogo />
+      {iconUrls.map((item) => (
+        // <img src={item} key={item} alt="" />
+        <SvgIcon name={item} key={item} width="50" height="50" />
+      ))}
       This is Header {hh}
     </p>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,20 @@ import windi from "vite-plugin-windicss";
 import viteEslint from "vite-plugin-eslint";
 import viteStylelint from "vite-plugin-stylelint";
 import svgr from "vite-plugin-svgr";
+import viteImagemin from "vite-plugin-imagemin";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 const variablePath = normalizePath(path.resolve("./src/css/variable.scss"));
+// 是否为 生产环境, 在生产环境 一般会注入 NODE_ENV 这个环境变量, 见下面的环境变量配置
+// .env.development NODE_ENV=development
+// .env.production NODE_ENV=production
+const isProduction = process.env.NODE_ENV === "production";
+
+const CDN_URL = "xxxxxx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProduction ? CDN_URL : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -18,6 +27,10 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "src/assets")
     },
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"]
+  },
+  build: {
+    // 8 kb 超过8kb的会单独打包成文件, 低于8kb的会作为 base64格式的字符串內联, svg始终会被打包成单独文件
+    assetsInlineLimit: 8 * 1024
   },
   plugins: [
     viteEslint(),
@@ -30,7 +43,31 @@ export default defineConfig({
     viteStylelint({
       exclude: /windicss|node_modules/
     }),
-    svgr()
+    svgr(),
+    viteImagemin({
+      // 无损压缩配置,无损压缩下图片质量不会变差
+      optipng: {
+        optimizationLevel: 7
+      },
+      // 有损压缩配置,有损压缩下图片质量可能会变差
+      pngquant: {
+        quality: [0.8, 0.9]
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox"
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false
+          }
+        ]
+      }
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [path.join(__dirname, "src/assets/icons")]
+    })
   ],
   css: {
     preprocessorOptions: {
