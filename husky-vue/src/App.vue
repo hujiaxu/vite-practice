7b8d3132d238ba383370e72993296b4d802c60a3
H <jxhunew@gmail.com>
2023-05-29 09:50:56 +0800

    vue project

---
+++ husky-vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <canvas id="canvas"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+let canvas = null;
+onMounted() {
+  canvas = document.getElementById('canvas');
+},
+</script>
+
+<style scoped></style>
